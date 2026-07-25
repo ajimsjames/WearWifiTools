074b88c88c2893bc16fd512af94e7f407888af25
Aju George <ajimsjames@gmail.com>
2026-07-26 00:18:10 +0530

    v1.5.1 release: High-tech custom Wi-Fi compass radar vector launcher icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.wearwifitools"
         minSdk = 30
         targetSdk = 33
-        versionCode = 9
-        versionName = "1.5.0"
+        versionCode = 10
+        versionName = "1.5.1"
 
         vectorDrawables {
             useSupportLibrary = true
