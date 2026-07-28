87947371e55327e52df51eec6b027d9e475d6f79
Aju George <ajimsjames@gmail.com>
2026-07-28 15:54:09 +0530

    Release v2.2.0: Bump version and compile official release binaries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.ajimsjames.wearwifitools"
         minSdk = 30
         targetSdk = 33
-        versionCode = 13
-        versionName = "2.1.0"
+        versionCode = 14
+        versionName = "2.2.0"
 
         vectorDrawables {
             useSupportLibrary = true
